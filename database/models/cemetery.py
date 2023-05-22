36842b375ae6966af813410f75458d091c9f1151
nixissmtp <gleb@simple2b.(none)>
2023-05-22 15:54:53 +0300

    each model in separate file + black

--- database/models/cemetery.py
+++ database/models/cemetery.py
@@ -2,31 +2,9 @@ import sqlalchemy as sa
 from sqlalchemy import orm
 
 from app.database import db
+
 from .utils import generate_uuid
-
-
-class CemeteryAudioTour(db.Model):
-    __tablename__ = "cemetery_audio_tours"
-
-    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
-    uuid: orm.Mapped[str] = orm.mapped_column(
-        sa.String(36),
-        default=generate_uuid,
-        index=True,
-    )
-
-    aws_filepath: orm.Mapped[str] = orm.mapped_column(sa.String(256))
-
-    cemetery_id: orm.Mapped[int] = orm.mapped_column(
-        sa.ForeignKey("cemeteries.id"), nullable=True
-    )
-
-
-class War(db.Model):
-    __tablename__ = "wars"
-
-    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
-    name: orm.Mapped[str] = orm.mapped_column(sa.String(64), nullable=False)
+from .cemetery_audio_tour import CemeteryAudioTour
 
 
 class Cemetery(db.Model):
---
+++ database/models/war.py
@@ -0,0 +1,11 @@
+import sqlalchemy as sa
+from sqlalchemy import orm
+
+from app.database import db
+
+
+class War(db.Model):
+    __tablename__ = "wars"
+
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    name: orm.Mapped[str] = orm.mapped_column(sa.String(64), nullable=False)
